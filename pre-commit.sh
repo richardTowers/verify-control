72a787e120027e20d5561924515fddce28ac0b18
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-06-17 14:16:54 +0100

    bau: Fix sed expressions for compat with BSD sed

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,6 +9,6 @@ skipped="$(grep '<testsuite' target/surefire-reports/TEST-* | grep -Eo 'skipped=
 
 percentage_complete="$(bc <<< "($tests - $skipped) * 100 / $tests")"
 
-sed -i '' "s/[0-9]\+% Done/$percentage_complete% Done/" README.md
-sed -i '' "s|http://progressed.io/bar/[0-9]\+|http://progressed.io/bar/$percentage_complete|" README.md
+sed -E -i '' "s/[0-9]+% Done/$percentage_complete% Done/" README.md
+sed -E -i '' "s|http://progressed.io/bar/[0-9]+|http://progressed.io/bar/$percentage_complete|" README.md
 
