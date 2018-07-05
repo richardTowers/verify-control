055a52413aa9c416a3c5ebe19ef831343d3f5536
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-07-05 16:00:31 +0100

    Be honest about the things matching things we haven't implemented yet

--- src/main/java/uk/gov/ida/hub/control/resources/MatchingServiceResponseResource.java
+++ src/main/java/uk/gov/ida/hub/control/resources/MatchingServiceResponseResource.java
@@ -6,6 +6,8 @@ import uk.gov.ida.hub.control.clients.SamlEngineClient;
 import uk.gov.ida.hub.control.clients.SessionClient;
 import uk.gov.ida.hub.control.errors.ApiBadRequestException;
 import uk.gov.ida.hub.control.errors.SessionNotFoundException;
+import uk.gov.ida.hub.control.errors.StateProcessingException;
+import uk.gov.ida.hub.control.statechart.VerifySessionState;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -35,16 +37,26 @@ public class MatchingServiceResponseResource {
     @POST
     @Timed
     public Response receiveAttributeQueryResponseFromMatchingService(@PathParam("sessionId") String sessionId, Map<String, String> samlResponse) throws SessionNotFoundException, ApiBadRequestException {
-        var session = sessionClient.getAll(sessionId);
+        var state = sessionClient.getState(sessionId);
         var translatedResponse = samlEngineClient.translateMatchingServiceResponse(samlResponse.get("samlResponse"));
         var status = translatedResponse.get("status");
         switch (status) {
             case "MatchingServiceMatch":
                 return handleMatchResponse(sessionClient, sessionId);
             case "NoMatchingServiceMatchFromMatchingService":
-                // TODO ... if cycle 3 is configured:
-                return handleAwaitCycle3Data(sessionClient, sessionId);
-            // TODO: other cases
+                switch (state.getName()) {
+                    case VerifySessionState.Cycle0And1MatchRequestSent.NAME:
+                        if (true) { // TODO if cycle 3 is configured too
+                            return handleAwaitCycle3Data(sessionClient, sessionId);
+                        }
+                        else {
+                            throw new NotImplementedException("No match for cycle 0 and 1"); // TODO
+                        }
+                    case VerifySessionState.Cycle3MatchRequestSent.NAME:
+                        throw new NotImplementedException("No match for cycle 3"); // TODO
+                    default:
+                        throw new StateProcessingException("NoMatchingServiceMatchFromMatchingService", state);
+                }
             default:
                 throw new NotImplementedException("Unknown matching status: " + status);
         }
